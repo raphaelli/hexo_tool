ff2323fce3100d906737bef57055dfd2d4ed87b4
raphaelli <raphael_li@live.com>
2018-11-05 08:47:45 +0800

    Update

--- NewAndEdit.sh
+++ NewAndEdit.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "Please input the article name:" && read name && hexo new $name && echo $name" add completed" && code "./source/_posts/"$name".md" && read
+echo "Please input the article name:" && read name && echo "log:hexo new" $name && hexo new $name && echo $name" add completed！" && code "./source/_posts/"$name".md" && read
